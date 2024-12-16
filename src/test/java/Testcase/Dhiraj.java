e64a19d62bf36042e431203684ce89923a1c2d10
dhira <dhira@DHIRAJ>
2024-12-16 17:12:55 +0530

    Dhiraj

--- src/test/java/Testcase/Dhiraj.java
+++ src/test/java/Testcase/Dhiraj.java
@@ -1,11 +1,10 @@
 package Testcase;
 
-import org.testng.reporters.jq.Main;
-
 public class Dhiraj 
 {
 	public static void main(String[] args) {
 		System.out.println("Dhiraj");
+		System.out.println("Dhirajkryadav");
 	}
 
 }
